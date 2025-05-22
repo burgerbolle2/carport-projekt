a2f01fd2b5d244a0c992afb50ff139380e64f81e
Bubberslem <Rasmussenm0@gmail.com>
2025-05-22 18:00:37 +0200

    login and create user updated

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -14,8 +14,8 @@ public class Main {
     private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
 
     private static final String USER = "postgres";
-    private static final String PASSWORD = "postgres";
-    private static final String URL = "jdbc:postgresql://localhost:5432/%s?currentSchema=public";
+    private static final String PASSWORD = System.getenv("password");
+    private static final String URL = "jdbc:postgresql://" +System.getenv("ip") + ":5432/%s?currentSchema=public";
     private static final String DB = "FOG";
 
     public static final ConnectionPool connectionPool = ConnectionPool.getInstance(USER, PASSWORD, URL, DB);
